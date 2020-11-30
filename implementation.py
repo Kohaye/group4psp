d860ddb3cc6d1929efe8324f01dd0a755072d254
Natalia Hrynko <nhrynko@flemingcollege.ca>
2020-11-30 14:50:11 -0500

    vdi check

--- implementation.py
+++ implementation.py
@@ -1,3 +1,4 @@
 # Group 4 Problem Solving and Programming Python Script file
 # Adding this comment in VS code at 2:10pm -> Korey Hayes
+#import matplotlib availible on college system as accessed through vdi
 # Dana like Pandas
