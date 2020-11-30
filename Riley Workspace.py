7048c1da6e5d86bc5bb4b99aac6b421718d2ac94
rileysweeneyfleming <riley.sweeney@flemingcollege.ca>
2020-11-30 16:24:10 -0500

    copying working file into workspace

--- Riley Workspace.py
+++ Riley Workspace.py
@@ -9,10 +9,10 @@
 # yearly totals at a specific monitoring station - St. Lawrence River at Cornwall
 import csv                                  # csv used in week 12 for csv file reading/writing
 import matplotlib                           # (available on college system as indicated on VDI)
-flow = open(r"StLawrenceFlow.csv")          # opens the file required to read the input
-freader = list(csv.reader(flow))			# sets the file that is read to a list of lists													# closes the file once the reader is done
-x = 1	
-y = 0								    # sets x to 1 because we don't want the first row of the csv                                      # sets y to 0 so we can compare yearChoice with index0 of freader
+flow = open(r"C:\PSP\Group4\group4psp\StLawrenceFlow.csv")          # opens the file required to read the input
+freader = list(csv.reader(flow))			# sets the file that is read to a list of lists					
+flow.close()								# closes the file once the reader is done
+x = 1									    # sets x to 1 because we don't want the first row of the csv
 janflow =[]                                 # create an empty list for each month of the year
 febflow =[]
 marflow =[]
@@ -27,20 +27,32 @@ novflow =[]
 decflow =[]
 yearflow =[]                                # create an empty list for the flow of the whole year
 # year number index = 0, jan = 1, feb = 2, ...... , dec = 12
-yearChoice = int(input("Please enter the year you would like to calculate the flow rate for:  "))
-for x in enumerate(freader):
-    if freader[x][y] == yearChoice:
-        janflow.append(freader[x][y+1])
-        febflow.append(freader[x][y+2]) 
-        marflow.append(freader[x][y+3]) 
-        aprflow.append(freader[x][y+4]) 
-        mayflow.append(freader[x][y+5]) 
-        junflow.append(freader[x][y+6]) 
-        julflow.append(freader[x][y+7]) 
-        augflow.append(freader[x][y+8]) 
-        sepflow.append(freader[x][y+9]) 
-        octflow.append(freader[x][y+10]) 
-        novflow.append(freader[x][y+11]) 
-        decflow.append(freader[x][y+12]) 
-        # yearflow.append(freader[x][y+1]) 
-print(janflow)
+yearChoice = input("Please enter the year you would like to calculate the flow rate for:  ")
+while x<1115:
+    if freader[x][0] == yearChoice:
+        if freader[x][1] != "":       # only append the list if there exists a value at this index
+            janflow.append(int(freader[x][1]))
+        if freader[x][2] !="":
+            febflow.append(int(freader[x][2]))
+        if freader[x][3] !="":
+            marflow.append(int(freader[x][3]))
+        if freader[x][4] !="":
+            aprflow.append(int(freader[x][4]))
+        if freader[x][5] !="":
+            mayflow.append(int(freader[x][5]))
+        if freader[x][6] !="":
+            junflow.append(int(freader[x][6]))
+        if freader[x][7] !="":
+            julflow.append(int(freader[x][7]))
+        if freader[x][8] !="":
+            augflow.append(int(freader[x][8]))
+        if freader[x][9] !="":
+            sepflow.append(int(freader[x][9]))
+        if freader[x][10] !="":
+            octflow.append(int(freader[x][10]))
+        if freader[x][11] !="":
+            novflow.append(int(freader[x][11]))
+        if freader[x][12] !="":
+            decflow.append(int(freader[x][12]))
+    x += 1
+print(janflow, '\n', febflow, '\n',marflow, '\n',aprflow, '\n',mayflow, '\n',junflow, '\n',julflow, '\n',augflow, '\n',sepflow, '\n',octflow, '\n',novflow, '\n',decflow)
