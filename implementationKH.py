7e72ef65c071d479061833df874b64e6142073f6
Korey <korey.hayes@flemingcollege.ca>
2020-11-30 16:23:13 -0500

    IT WORKS

--- implementationKH.py
+++ implementationKH.py
@@ -27,23 +27,32 @@ novflow =[]
 decflow =[]
 yearflow =[]                                # create an empty list for the flow of the whole year
 # year number index = 0, jan = 1, feb = 2, ...... , dec = 12
-yearChoice = int(input("Please enter the year you would like to calculate the flow rate for:  "))
-print(freader[1][0])
+yearChoice = input("Please enter the year you would like to calculate the flow rate for:  ")
 while x<1115:
     if freader[x][0] == yearChoice:
-        print("maybe this is why")
-        janflow.append(int(freader[x][1]))
-        febflow.append(int(freader[x][2]))
-        marflow.append(int(freader[x][3])) 
-        aprflow.append(int(freader[x][4])) 
-        mayflow.append(int(freader[x][5]))
-        junflow.append(int(freader[x][6])) 
-        julflow.append(int(freader[x][7])) 
-        augflow.append(int(freader[x][8])) 
-        sepflow.append(int(freader[x][9])) 
-        octflow.append(int(freader[x][10])) 
-        novflow.append(int(freader[x][11])) 
-        decflow.append(int(freader[x][12])) 
-        # yearflow.append(freader[x][y+1])
+        if freader[x][1] != "":       # only append the list if there exists a value at this index
+            janflow.append(int(freader[x][1]))
+        if freader[x][2] !="":
+            febflow.append(int(freader[x][2]))
+        if freader[x][3] !="":
+            marflow.append(int(freader[x][3]))
+        if freader[x][4] !="":
+            aprflow.append(int(freader[x][4]))
+        if freader[x][5] !="":
+            mayflow.append(int(freader[x][5]))
+        if freader[x][6] !="":
+            junflow.append(int(freader[x][6]))
+        if freader[x][7] !="":
+            julflow.append(int(freader[x][7]))
+        if freader[x][8] !="":
+            augflow.append(int(freader[x][8]))
+        if freader[x][9] !="":
+            sepflow.append(int(freader[x][9]))
+        if freader[x][10] !="":
+            octflow.append(int(freader[x][10]))
+        if freader[x][11] !="":
+            novflow.append(int(freader[x][11]))
+        if freader[x][12] !="":
+            decflow.append(int(freader[x][12]))
     x += 1
-print(janflow)
+print(janflow, '\n', febflow, '\n',marflow, '\n',aprflow, '\n',mayflow, '\n',junflow, '\n',julflow, '\n',augflow, '\n',sepflow, '\n',octflow, '\n',novflow, '\n',decflow)
