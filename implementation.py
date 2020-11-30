9442786c543c78d842b38dc6eb685143fa100f34
Korey <korey.hayes@flemingcollege.ca>
2020-11-30 15:03:13 -0500

    j

--- implementation.py
+++ implementation.py
@@ -1,6 +1,5 @@
 # Group 4 Problem Solving and Programming Python Script file
 # Adding this comment in VS code at 2:10pm -> Korey Hayes
-<<<<<<< HEAD
 # Dan like Pandas
 # Korey updated the CSV file to only contain relevant information
 # to the program columns = year and month of that year
@@ -24,7 +23,3 @@ octflow = []
 novflow = []
 decflow = []
 yearflow = []                               # create an empty list for the flow of the whole year
-=======
-#import matplotlib availible on college system as accessed through vdi
-# Dana like Pandas
->>>>>>> d860ddb3cc6d1929efe8324f01dd0a755072d254
